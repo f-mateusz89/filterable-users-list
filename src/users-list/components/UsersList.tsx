478f120fbfc27a1e45ce5b393c38199be974faba
Mateusz Filip <mateusz.filip@pronos.eu>
2020-02-22 13:58:41 +0100

    Add filter field

--- src/users-list/components/UsersList.tsx
+++ src/users-list/components/UsersList.tsx
@@ -1,11 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import axios from 'axios';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: 200,
+      },
+    },
+  }),
+);
 
 function UsersList(): JSX.Element {
   const [users, setUsers] = useState([]);
+  const classes = useStyles();
 
   useEffect(() => {
     async function fetchUsers(): Promise<void> {
@@ -21,18 +35,24 @@ function UsersList(): JSX.Element {
   }, []);
 
   return (
-    <List dense>
-      {users.map(
-        ({ id, name, username }, index): React.ReactElement => (
-          <ListItem key={id}>
-            <ListItemText
-              primary={`${index + 1}. ${name}`}
-              secondary={username}
-            />
-          </ListItem>
-        ),
-      )}
-    </List>
+    <>
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField label="Search by user name..." />
+      </form>
+
+      <List dense>
+        {users.map(
+          ({ id, name, username }, index): React.ReactElement => (
+            <ListItem key={id}>
+              <ListItemText
+                primary={`${index + 1}. ${name}`}
+                secondary={`@${username}`}
+              />
+            </ListItem>
+          ),
+        )}
+      </List>
+    </>
   );
 }
 
